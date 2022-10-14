24ab540c8e874f8dca12b664c4ae786afa9e1ca5
Luiz Eduardo <82010335+leagueliine@users.noreply.github.com>
2022-10-14 20:16:51 -0300

    Update index.js

--- src/JavaScript/index.js
+++ src/JavaScript/index.js
@@ -36,7 +36,7 @@ let typeWriter = function (elemento) {
   const textArray = elemento.innerHTML.split('');
   elemento.innerHTML = '';
   textArray.forEach((letra, i) => {
-    setTimeout( () => elemento.innerHTML += letra, 75 * i)
+    setTimeout( () => elemento.innerHTML += letra, 60 * i)
   });
  };
 
